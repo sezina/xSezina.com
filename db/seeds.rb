fad6f0100db05aad52ad5b708cfc79635099209c
sezina <sezinamd@gmail.com>
2012-08-01 11:48:12 +0800

    before add category

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
+Category.create [{:name => 'Programming'},
+                 {:name => 'Python'},
+                 {:name => 'Ruby'}, {:name => 'Linux'}]
